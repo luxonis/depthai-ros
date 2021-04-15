1b9dc46308be084b3fdf35a76a1ccb8fd4ff5e1b
Sachin <sachin.guruswamy@gmail.com>
2021-04-15 12:16:25 -0700

    added dependency script fixes 2

--- install_dependencis.sh
+++ install_dependencis.sh
@@ -1,9 +1,12 @@
 sudo wget -qO- http://docs.luxonis.com/_static/install_dependencies.sh | bash
 cd /tmp
 git clone --recursive https://github.com/luxonis/depthai-core.git --branch develop
+cd depthai-core
 mkdir build
 cd build
 cmake .. -DBUILD_SHARED_LIBS=ON
 cd ..
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build build --target install
+cd /tmp
+rm -r depthai-core
