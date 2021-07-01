21baff8e923e7ea0df60a2138174e5d14d306fdd
Sachin <sachin.guruswamy@gmail.com>
2021-06-30 20:24:24 -0700

    Updated Install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,9 +2,7 @@ set -e
 sudo wget -qO- http://docs.luxonis.com/_static/install_dependencies.sh | bash
 cd /tmp
 git clone --recursive https://github.com/luxonis/depthai-core.git --branch ros2-main-gen2
-cd depthai-core
-mkdir build
-cmake -S . -B build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local
-cmake --build build --target install
+cmake -Hdepthai-core -Bdepthai-core/build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local
+cmake --build depthai-core/build --target install
 cd /tmp
 rm -r depthai-core
