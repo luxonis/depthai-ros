b5ffa8426acf26966d672d9bc5e3b710b315b559
saching13 <sachin.guruswamy@gmail.com>
2021-07-28 13:39:27 -0700

    Added formatting

--- depthai_bridge/include/depthai_bridge/ImageConverter.hpp
+++ depthai_bridge/include/depthai_bridge/ImageConverter.hpp
@@ -1,49 +1,51 @@
 #pragma once
 
-#include <sstream>
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <sstream>
 #include <unordered_map>
 
-#include "sensor_msgs/Image.h"
-#include "sensor_msgs/CameraInfo.h"
 #include "depthai-shared/common/CameraBoardSocket.hpp"
-
 #include "depthai/depthai.hpp"
-#include <opencv2/opencv.hpp>
+#include "sensor_msgs/CameraInfo.h"
+#include "sensor_msgs/Image.h"
 
 namespace dai::rosBridge {
 using TimePoint = std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration>;
 
-class ImageConverter{
-
-    public:
+class ImageConverter {
+   public:
     // ImageConverter() = default;
     ImageConverter(const std::string frameName, bool interleaved);
     ImageConverter(bool interleaved);
-    
+
     void toRosMsg(std::shared_ptr<dai::ImgFrame> inData, sensor_msgs::Image& outImageMsg);
     sensor_msgs::ImagePtr toRosMsgPtr(std::shared_ptr<dai::ImgFrame> inData);
 
     void toDaiMsg(const sensor_msgs::Image& inMsg, dai::ImgFrame& outData);
 
-    /** TODO(sachin): Add support for ros msg to cv mat since we have some 
-     *  encodings which cv supports but ros doesn't    
-    **/
+    /** TODO(sachin): Add support for ros msg to cv mat since we have some
+     *  encodings which cv supports but ros doesn't
+     **/
     cv::Mat rosMsgtoCvMat(sensor_msgs::Image& inMsg);
 
-    sensor_msgs::CameraInfo calibrationToCameraInfo(dai::CalibrationHandler calibHandler, dai::CameraBoardSocket cameraId, int width = -1, int height = -1, Point2f topLeftPixelId = Point2f(), Point2f bottomRightPixelId = Point2f());
+    sensor_msgs::CameraInfo calibrationToCameraInfo(dai::CalibrationHandler calibHandler,
+                                                    dai::CameraBoardSocket cameraId,
+                                                    int width = -1,
+                                                    int height = -1,
+                                                    Point2f topLeftPixelId = Point2f(),
+                                                    Point2f bottomRightPixelId = Point2f());
 
-     private:
-        static std::unordered_map<dai::RawImgFrame::Type, std::string> encodingEnumMap;
-        static std::unordered_map<dai::RawImgFrame::Type, std::string> planarEncodingEnumMap;
+   private:
+    static std::unordered_map<dai::RawImgFrame::Type, std::string> encodingEnumMap;
+    static std::unordered_map<dai::RawImgFrame::Type, std::string> planarEncodingEnumMap;
 
     // dai::RawImgFrame::Type _srcType;
     bool _daiInterleaved;
     // bool c
     const std::string _frameName = "";
-    void planarToInterleaved(const std::vector<uint8_t>& srcData, std::vector<uint8_t>& destData, int w, int h , int numPlanes, int bpp);
-    void interleavedToPlanar(const std::vector<uint8_t>& srcData, std::vector<uint8_t>& destData, int w, int h , int numPlanes, int bpp);
-
+    void planarToInterleaved(const std::vector<uint8_t>& srcData, std::vector<uint8_t>& destData, int w, int h, int numPlanes, int bpp);
+    void interleavedToPlanar(const std::vector<uint8_t>& srcData, std::vector<uint8_t>& destData, int w, int h, int numPlanes, int bpp);
 };
 
-}   // namespace dai::rosBridge
+}  // namespace dai::rosBridge
--- depthai_bridge/include/depthai_bridge/SpatialDetectionConverter.hpp
+++ depthai_bridge/include/depthai_bridge/SpatialDetectionConverter.hpp
@@ -1,40 +1,42 @@
-#include "depthai/depthai.hpp"
-#include "sensor_msgs/Image.h"
-#include <depthai_bridge/ImageConverter.hpp>
 #include <depthai_ros_msgs/SpatialDetectionArray.h>
 #include <vision_msgs/Detection2DArray.h>
 
 #include <boost/make_shared.hpp>
 #include <boost/shared_ptr.hpp>
+#include <depthai_bridge/ImageConverter.hpp>
+
+#include "depthai/depthai.hpp"
+#include "sensor_msgs/Image.h"
 
 namespace dai::rosBridge {
 
 class SpatialDetectionConverter {
-public:
-  // DetectionConverter() = default;
-  SpatialDetectionConverter(std::string frameName, int width, int height, bool normalized = false);
+   public:
+    // DetectionConverter() = default;
+    SpatialDetectionConverter(std::string frameName, int width, int height, bool normalized = false);
 
-  void toRosMsg(std::shared_ptr<dai::SpatialImgDetections> inNetData,
-           depthai_ros_msgs::SpatialDetectionArray &opDetectionMsg, TimePoint tStamp, int32_t sequenceNum = -1);
+    void toRosMsg(std::shared_ptr<dai::SpatialImgDetections> inNetData,
+                  depthai_ros_msgs::SpatialDetectionArray& opDetectionMsg,
+                  TimePoint tStamp,
+                  int32_t sequenceNum = -1);
 
-  void toRosMsg(std::shared_ptr<dai::SpatialImgDetections> inNetData,
-           depthai_ros_msgs::SpatialDetectionArray &opDetectionMsg);
+    void toRosMsg(std::shared_ptr<dai::SpatialImgDetections> inNetData, depthai_ros_msgs::SpatialDetectionArray& opDetectionMsg);
 
-depthai_ros_msgs::SpatialDetectionArray::Ptr toRosMsgPtr(std::shared_ptr<dai::SpatialImgDetections> inNetData);
+    depthai_ros_msgs::SpatialDetectionArray::Ptr toRosMsgPtr(std::shared_ptr<dai::SpatialImgDetections> inNetData);
 
-private:
-  uint32_t _sequenceNum;
-  int _width, _height;
-  const std::string _frameName;
-  bool _normalized;
+   private:
+    uint32_t _sequenceNum;
+    int _width, _height;
+    const std::string _frameName;
+    bool _normalized;
 };
 
-  /** TODO(sachin): Do we need to have ros msg -> dai bounding box ?
-   * is there any situation where we would need to have xlinkin to take bounding
-   * box as input. One scenario would to take this as input and use ImageManip
-   * node to crop the roi of the image. Since it is not available yet. Leaving
-   * it out for now to speed up on other tasks feel free to raise a issue if you
-   * feel that feature is good to have...
-   */
+/** TODO(sachin): Do we need to have ros msg -> dai bounding box ?
+ * is there any situation where we would need to have xlinkin to take bounding
+ * box as input. One scenario would to take this as input and use ImageManip
+ * node to crop the roi of the image. Since it is not available yet. Leaving
+ * it out for now to speed up on other tasks feel free to raise a issue if you
+ * feel that feature is good to have...
+ */
 
-}   // namespace dai::rosBridge
+}  // namespace dai::rosBridge
