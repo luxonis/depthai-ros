f618a5c13c2628f17ca742102a5d4123734f1dff
nindanaoto <matsuoka.kotaro@gmail.com>
2021-10-19 13:11:13 +0000

    Added IMU support

---
+++ depthai_bridge/include/depthai_bridge/ImuConverter.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <sstream>
+#include <unordered_map>
+
+#include "depthai/depthai.hpp"
+#include "sensor_msgs/Imu.h"
+
+namespace dai::rosBridge {
+using TimePoint = std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration>;
+
+class ImuConverter {
+   public:
+    ImuConverter(const std::string& frameName);
+
+    void toRosMsg(std::shared_ptr<dai::IMUData> inData, sensor_msgs::Imu& outImuMsg);
+    sensor_msgs::Imu::Ptr toRosMsgPtr(const std::shared_ptr<dai::IMUData> inData);
+
+   private:
+    uint32_t _sequenceNum;
+    const std::string _frameName = "";
+};
+
+}  // namespace dai::rosBridge
