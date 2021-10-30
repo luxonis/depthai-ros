52257cfa64a83a56d865fa6acc327b90aa284dec
Sachin <sachin.guruswamy@gmail.com>
2021-10-29 22:01:14 -0700

    changed core to develop

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 set -e
 sudo wget -qO- http://docs.luxonis.com/_static/install_dependencies.sh | bash
 cd /tmp
-git clone --recursive https://github.com/luxonis/depthai-core.git --branch ros2-main-gen2
+git clone --recursive https://github.com/luxonis/depthai-core.git --branch develop
 cmake -Hdepthai-core -Bdepthai-core/build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build depthai-core/build --target install
 cd /tmp
