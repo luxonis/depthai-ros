b170f74a67dd0e663092eeb95924b2fc631b0f6e
Sachin Guruswamy <43363595+saching13@users.noreply.github.com>
2021-12-05 07:48:08 -0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 set -e
 sudo wget -qO- http://docs.luxonis.com/_static/install_dependencies.sh | bash
 cd /tmp
-git clone --recursive https://github.com/luxonis/depthai-core.git --branch develop
+git clone --recursive https://github.com/luxonis/depthai-core.git --branch main
 cmake -Hdepthai-core -Bdepthai-core/build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build depthai-core/build --target install
 cd /tmp
