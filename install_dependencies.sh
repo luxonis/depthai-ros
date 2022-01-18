374086809e79993909d7fc8aed86e523aa83e278
Sachin Guruswamy <43363595+saching13@users.noreply.github.com>
2022-01-18 15:29:32 -0800

    Reverting to using old script till dependencies issues are resolved.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 set -e
-sudo wget -qO- http://docs.luxonis.com/_static/install_dependencies.sh | bash
+sudo wget -qO- https://raw.githubusercontent.com/luxonis/depthai-docs-website/master/source/_static/install_dependencies.sh | bash 
 cd /tmp
 git clone --recursive https://github.com/luxonis/depthai-core.git --branch main
 cmake -Hdepthai-core -Bdepthai-core/build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local
