f38561640a32afaa79a9457cf5ee494a8e422d6e
Kaiden <kaidenfield898@yahoo.com>
2022-03-08 19:35:16 -0500

    added feature to publish IMU transform

--- depthai_bridge/include/depthai_bridge/ImuConverter.hpp
+++ depthai_bridge/include/depthai_bridge/ImuConverter.hpp
@@ -7,9 +7,14 @@
 
 #include "depthai/depthai.hpp"
 
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 #ifdef IS_ROS2
     #include "rclcpp/rclcpp.hpp"
     #include "sensor_msgs/msg/imu.hpp"
+    #include <geometry_msgs/msg/transform_stamped.hpp>
 #else
     #include <ros/ros.h>
 
@@ -25,20 +30,47 @@ namespace ros {
 #ifdef IS_ROS2
 namespace ImuMsgs = sensor_msgs::msg;
 using ImuPtr = ImuMsgs::Imu::SharedPtr;
+namespace rosOrigin = ::rclcpp;
+namespace GeometryMsg = geometry_msgs::msg;
 #else
 namespace ImuMsgs = sensor_msgs;
 using ImuPtr = ImuMsgs::Imu::Ptr;
+namespace rosOrigin = ::ros;
+namespace GeometryMsg = geometry_msgs;
 #endif
 class ImuConverter {
    public:
-    ImuConverter(const std::string& frameName);
+#ifdef IS_ROS2
+    ImuConverter(const std::string& frameName,
+                 std::shared_ptr<rosOrigin::Node> node,
+                 const bool publishTransform = false, 
+                 const std::string base_frame = "oak-d-base-frame",
+                 const std::string world_frame = "map");
+#else
+    ImuConverter(const std::string& frameName,
+                 const bool publishTransform = false, 
+                 const std::string base_frame = "oak-d-base-frame",
+                 const std::string world_frame = "map");
+#endif
 
     void toRosMsg(std::shared_ptr<dai::IMUData> inData, ImuMsgs::Imu& outImuMsg);
     ImuPtr toRosMsgPtr(const std::shared_ptr<dai::IMUData> inData);
 
    private:
+    void publishtransform(ImuMsgs::Imu& outImuMsg);
     uint32_t _sequenceNum;
     const std::string _frameName = "";
+    const bool _publishTransform = false;
+    const std::string _base_frame = "";
+    const std::string _world_frame = "";
+    GeometryMsg::TransformStamped imuToOakBase;
+    std::unique_ptr<tf2_ros::Buffer> tfBuffer = nullptr;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> br = nullptr;
+    std::shared_ptr<tf2_ros::TransformListener> tfListener = nullptr;
+
+    #ifdef IS_ROS2
+    std::shared_ptr<rosOrigin::Node> _node = nullptr;
+    #endif
 };
 
 }  // namespace ros
--- depthai_bridge/src/ImuConverter.cpp
+++ depthai_bridge/src/ImuConverter.cpp
@@ -4,8 +4,53 @@
 namespace dai {
 
 namespace ros {
+#ifdef IS_ROS2
+    ImuConverter::ImuConverter(const std::string& frameName,
+                               std::shared_ptr<rosOrigin::Node> node,
+                               const bool publishTransform, 
+                               const std::string base_frame,
+                               const std::string world_frame) 
+: _frameName(frameName),
+  _node(node),
+  _publishTransform(publishTransform), 
+  _base_frame(base_frame),
+  _world_frame(world_frame), 
+  _sequenceNum(0) {
+    if(_publishTransform) {
+        tfBuffer = std::make_unique<tf2_ros::Buffer>(_node->get_clock());
+        tfListener = std::make_shared<tf2_ros::TransformListener>(*tfBuffer);
+        br = std::make_unique<tf2_ros::TransformBroadcaster>(*node);
 
-ImuConverter::ImuConverter(const std::string& frameName) : _frameName(frameName), _sequenceNum(0) {}
+        while(!tfBuffer->canTransform(_frameName, _base_frame, tf2::TimePointZero) && rclcpp::ok()) {
+            RCLCPP_INFO(node->get_logger(), tfBuffer->allFramesAsString());
+            RCLCPP_INFO(
+                    node->get_logger(), "waiting for IMU transform to become available");
+            rclcpp::sleep_for(std::chrono::milliseconds(30));
+        }
+        RCLCPP_INFO(node->get_logger(), "transform available");
+
+        imuToOakBase  = tfBuffer->lookupTransform(_frameName, _base_frame, tf2::TimePointZero);
+    }
+}
+#else
+    ImuConverter::ImuConverter(const std::string& frameName,
+                               const bool publishTransform, 
+                               const std::string base_frame,
+                               const std::string world_frame) 
+    : _frameName(frameName),
+      _publishTransform(publishTransform), 
+      _base_frame(base_frame),
+      _world_frame(world_frame), 
+      _sequenceNum(0) {
+        if(_publishTransform) {
+            tfBuffer = std::make_unique<tf2_ros::Buffer>();
+            tfListener = std::make_shared<tf2_ros::TransformListener>(*tfBuffer);
+            br = std::make_unique<tf2_ros::TransformBroadcaster>();
+            
+            imuToOakBase  = tfBuffer->lookupTransform(_frameName, _base_frame, rosOrigin::Time(0), rosOrigin::Duration(1.0) );
+        }
+    }
+#endif
 
 void ImuConverter::toRosMsg(std::shared_ptr<dai::IMUData> inData, ImuMsgs::Imu& outImuMsg) {
 // setting the header
@@ -46,6 +91,10 @@ void ImuConverter::toRosMsg(std::shared_ptr<dai::IMUData> inData, ImuMsgs::Imu& 
     }
 
     _sequenceNum++;
+
+    if(_publishTransform) {
+        publishtransform(outImuMsg);
+    }
 }
 
 ImuPtr ImuConverter::toRosMsgPtr(const std::shared_ptr<dai::IMUData> inData) {
@@ -57,6 +106,28 @@ ImuPtr ImuConverter::toRosMsgPtr(const std::shared_ptr<dai::IMUData> inData) {
 
     toRosMsg(inData, *ptr);
     return ptr;
+}
+
+void ImuConverter::publishtransform(ImuMsgs::Imu& outImuMsg){
+    GeometryMsg::TransformStamped transformStamped;
+
+    transformStamped.header.stamp = outImuMsg.header.stamp;
+    transformStamped.header.frame_id = _world_frame;
+
+    transformStamped.child_frame_id = _base_frame;
+
+    transformStamped.transform.rotation.x = outImuMsg.orientation.z;
+    transformStamped.transform.rotation.y = outImuMsg.orientation.y;
+    transformStamped.transform.rotation.z = outImuMsg.orientation.x * -1;
+    transformStamped.transform.rotation.w = outImuMsg.orientation.w;
+
+    tf2::doTransform(transformStamped, transformStamped, imuToOakBase);
+    transformStamped.header.stamp = outImuMsg.header.stamp;
+    transformStamped.header.frame_id = _world_frame;
+
+    transformStamped.child_frame_id = _base_frame;
+
+    br->sendTransform(transformStamped);
 }
 
 }  // namespace ros
