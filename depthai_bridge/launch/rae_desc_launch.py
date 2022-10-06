579e786735018cfb746679fe0e5fa3577e926fab
saching13 <sachin.guruswamy@gmail.com>
2022-10-06 14:17:15 -0400

    added rae models WIP

---
+++ depthai_bridge/launch/rae_desc_launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    bringup_dir = get_package_share_directory('depthai_bridge')
+    xacro_path = os.path.join(bringup_dir, 'urdf', 'rae.urdf.xacro')
+
+
+    rsp_node =  Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='oak_state_publisher',
+            parameters=[{'robot_description': Command(
+                [
+                    'xacro', ' ', xacro_path, ' ',
+                ])}]
+        )
+    
+    ld = LaunchDescription()
+    ld.add_action(rsp_node)
+    return ld
