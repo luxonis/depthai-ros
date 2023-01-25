a3e585e9348f461dab6388e956848d604fb9b1ce
Horst Dumcke <hdumcke@marlysys.com>
2023-01-25 17:15:01 +0100

    cmake install requires root in install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,6 +5,6 @@ sudo udevadm control --reload-rules && sudo udevadm trigger
 cd /tmp
 git clone --recursive https://github.com/luxonis/depthai-core.git --branch main
 cmake -Hdepthai-core -Bdepthai-core/build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local
-cmake --build depthai-core/build --target install
+sudo cmake --build depthai-core/build --target install
 cd /tmp
 rm -r depthai-core
