776c21131b799511b0f695acd1851d0b9ac7bc6f
Borong Yuan <yuanborong@hotmail.com>
2023-06-02 17:01:17 +0800

    Update Depth Alignment

--- depthai_ros_driver/src/dai_nodes/stereo.cpp
+++ depthai_ros_driver/src/dai_nodes/stereo.cpp
@@ -32,7 +32,7 @@ Stereo::Stereo(const std::string& daiNodeName,
     right = std::make_unique<SensorWrapper>(rightInfo.name, node, pipeline, device, rightInfo.socket, false);
 
     ph = std::make_unique<param_handlers::StereoParamHandler>(node, daiNodeName);
-    ph->declareParams(stereoCamNode, rightInfo.name);
+    ph->declareParams(stereoCamNode, leftInfo.name, rightInfo.name);
     setXinXout(pipeline);
     left->link(stereoCamNode->left);
     right->link(stereoCamNode->right);
@@ -152,7 +152,17 @@ void Stereo::setupStereoQueue(std::shared_ptr<dai::Device> device) {
     stereoQ = device->getOutputQueue(stereoQName, ph->getParam<int>("i_max_q_size"), false);
     std::string tfPrefix;
     if(ph->getParam<bool>("i_align_depth")) {
-        tfPrefix = getTFPrefix("rgb");
+        switch(static_cast<dai::CameraBoardSocket>(ph->getParam<int>("i_board_socket_id"))) {
+            case dai::CameraBoardSocket::RGB:
+                tfPrefix = getTFPrefix("rgb");
+                break;
+            case dai::CameraBoardSocket::LEFT:
+                tfPrefix = getTFPrefix(leftSensInfo.name);
+                break;
+            default:
+                tfPrefix = getTFPrefix(rightSensInfo.name);
+                break;
+        }
     } else {
         tfPrefix = getTFPrefix(rightSensInfo.name);
     }
