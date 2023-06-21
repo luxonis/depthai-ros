657068bf1003e53ed720562fdc35d66c17e8eef4
Adam Serafin <adam.serafin@luxonis.com>
2023-06-21 18:42:48 +0200

    3D bounding boxes in Humble (#336)

--- depthai_filters/include/depthai_filters/spatial_bb.hpp
+++ depthai_filters/include/depthai_filters/spatial_bb.hpp
@@ -1,30 +1,38 @@
 #pragma once
 
+#include "visualization_msgs/msg/marker_array.hpp"
 #include "message_filters/subscriber.h"
 #include "message_filters/sync_policies/approximate_time.h"
 #include "message_filters/synchronizer.h"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "vision_msgs/msg/detection2_d_array.hpp"
+#include "vision_msgs/msg/detection3_d_array.hpp"
 
 namespace depthai_filters {
-class Detection2DOverlay : public rclcpp::Node {
+class SpatialBB : public rclcpp::Node {
    public:
-    explicit Detection2DOverlay(const rclcpp::NodeOptions& options);
+    explicit SpatialBB(const rclcpp::NodeOptions& options);
     void onInit();
 
-    void overlayCB(const sensor_msgs::msg::Image::ConstSharedPtr& preview, const vision_msgs::msg::Detection2DArray::ConstSharedPtr& detections);
+    void overlayCB(const sensor_msgs::msg::Image::ConstSharedPtr& preview,
+                   const sensor_msgs::msg::CameraInfo::ConstSharedPtr& info,
+                   const vision_msgs::msg::Detection3DArray::ConstSharedPtr& detections);
 
     message_filters::Subscriber<sensor_msgs::msg::Image> previewSub;
-    message_filters::Subscriber<vision_msgs::msg::Detection2DArray> detSub;
+    message_filters::Subscriber<vision_msgs::msg::Detection3DArray> detSub;
+    message_filters::Subscriber<sensor_msgs::msg::CameraInfo> infoSub;
 
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, vision_msgs::msg::Detection2DArray> syncPolicy;
+    typedef message_filters::sync_policies::
+        ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo, vision_msgs::msg::Detection3DArray>
+            syncPolicy;
     std::unique_ptr<message_filters::Synchronizer<syncPolicy>> sync;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markerPub;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr overlayPub;
     std::vector<std::string> labelMap = {"background", "aeroplane", "bicycle",     "bird",  "boat",        "bottle", "bus",
                                                "car",        "cat",       "chair",       "cow",   "diningtable", "dog",    "horse",
                                                "motorbike",  "person",    "pottedplant", "sheep", "sofa",        "train",  "tvmonitor"};
+    bool desqueeze = false;
 };
 
 }  // namespace depthai_filters
