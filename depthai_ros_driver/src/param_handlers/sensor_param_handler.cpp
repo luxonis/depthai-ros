ee9393610db3c8f87e8baf1614d5ff6b230b4521
Serafadam <serafadam@gmail.com>
2023-08-16 11:28:34 +0000

    setisp3a to 10fps by default

--- depthai_ros_driver/src/param_handlers/sensor_param_handler.cpp
+++ depthai_ros_driver/src/param_handlers/sensor_param_handler.cpp
@@ -64,6 +64,9 @@ void SensorParamHandler::declareParams(std::shared_ptr<dai::node::MonoCamera> mo
     if(declareAndLogParam<bool>("i_fsync_trigger", false)) {
         monoCam->initialControl.setExternalTrigger(declareAndLogParam<int>("i_num_frames_burst", 1), declareAndLogParam<int>("i_num_frames_discard", 0));
     }
+    if(declareAndLogParam<bool>("i_set_isp3a_fps", true)) {
+        monoCam->setIsp3aFps(declareAndLogParam<int>("i_isp3a_fps", 10));
+    }
 }
 void SensorParamHandler::declareParams(std::shared_ptr<dai::node::ColorCamera> colorCam,
                                        dai::CameraBoardSocket socket,
@@ -131,8 +134,8 @@ void SensorParamHandler::declareParams(std::shared_ptr<dai::node::ColorCamera> c
     if(declareAndLogParam<bool>("i_fsync_trigger", false)) {
         colorCam->initialControl.setExternalTrigger(declareAndLogParam<int>("i_num_frames_burst", 1), declareAndLogParam<int>("i_num_frames_discard", 0));
     }
-    if(declareAndLogParam<bool>("i_set_isp3a_fps", false)) {
-        colorCam->setIsp3aFps(declareAndLogParam<int>("i_isp3a_fps", 15));
+    if(declareAndLogParam<bool>("i_set_isp3a_fps", true)) {
+        colorCam->setIsp3aFps(declareAndLogParam<int>("i_isp3a_fps", 10));
     }
 }
 dai::CameraControl SensorParamHandler::setRuntimeParams(const std::vector<rclcpp::Parameter>& params) {
