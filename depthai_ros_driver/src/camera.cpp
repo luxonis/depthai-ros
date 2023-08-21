032f69e2379be9352215c5d614b0016bec3ba44b
Alexandre Chataignon <alexandre.github@xouillet.info>
2023-08-21 10:08:30 +0200

    Handle USB speed when usb id is specified (#372)
    Co-authored-by: Alexandre Chataignon <alexandre.chataignon@robocc.com>

--- depthai_ros_driver/src/camera.cpp
+++ depthai_ros_driver/src/camera.cpp
@@ -159,7 +159,7 @@ void Camera::startDevice() {
                     } else if(!usb_id.empty() && info.name == usb_id) {
                         RCLCPP_INFO(this->get_logger(), "Connecting to the camera using USB ID: %s", usb_id.c_str());
                         if(info.state == X_LINK_UNBOOTED || info.state == X_LINK_BOOTLOADER) {
-                            device = std::make_shared<dai::Device>(info);
+                            device = std::make_shared<dai::Device>(info, speed);
                             camRunning = true;
                         } else if(info.state == X_LINK_BOOTED) {
                             throw std::runtime_error("Device is already booted in different process.");
