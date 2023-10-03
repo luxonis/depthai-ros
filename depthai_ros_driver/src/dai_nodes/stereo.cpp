952f4c8538efee10cb839a9d8a0b5f81824db0ec
Serafadam <serafadam@gmail.com>
2023-10-03 10:42:46 +0000

    alpha scaling factor in intrinsics calculation

--- depthai_ros_driver/src/dai_nodes/stereo.cpp
+++ depthai_ros_driver/src/dai_nodes/stereo.cpp
@@ -213,6 +213,9 @@ void Stereo::setupStereoQueue(std::shared_ptr<dai::Device> device) {
     if(ph->getParam<bool>("i_reverse_stereo_socket_order")) {
         stereoConv->reverseStereoSocketOrder();
     }
+    if(ph->getParam<bool>("i_enable_alpha_scaling")){
+        stereoConv->setAlphaScaling(ph->getParam<double>("i_alpha_scaling"));
+    }
     stereoIM = std::make_shared<camera_info_manager::CameraInfoManager>(
         getROSNode()->create_sub_node(std::string(getROSNode()->get_name()) + "/" + getName()).get(), "/" + getName());
     auto info = sensor_helpers::getCalibInfo(getROSNode()->get_logger(),
