7f4956809fb363e7442fb435d3df479f553a0e09
Serafadam <serafadam@gmail.com>
2024-02-02 10:39:59 +0000

    color for tof stereo pair

--- depthai_ros_driver/src/dai_nodes/sensors/sensor_wrapper.cpp
+++ depthai_ros_driver/src/dai_nodes/sensors/sensor_wrapper.cpp
@@ -54,7 +54,7 @@ SensorWrapper::SensorWrapper(const std::string& daiNodeName,
         }
         RCLCPP_DEBUG(node->get_logger(), "Node %s has sensor %s", daiNodeName.c_str(), sensorName.c_str());
         sensorData = *sensorIt;
-        if(device->getDeviceName() == "OAK-D-SR") {
+        if(device->getDeviceName() == "OAK-D-SR" || device->getDeviceName() == "OAK-D-SR-POE"){
             (*sensorIt).color = true;  // ov9282 is color sensor in this case
         }
         if((*sensorIt).color) {
