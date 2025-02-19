12de403f5e207cc125b975faa505145dc9ae7c4e
Serafadam <adam.serafin@luxonis.com>
2025-02-19 11:44:31 +0000

    fix warnings

--- depthai_filters/src/thermal_temp.cpp
+++ depthai_filters/src/thermal_temp.cpp
@@ -17,7 +17,7 @@ void ThermalTemp::onInit() {
     colorPub = pNH.advertise<sensor_msgs::Image>("thermal_colormap", 10);
 }
 
-void ThermalTemp::mouseCallback(int event, int x, int y, int flags, void* userdata) {
+void ThermalTemp::mouseCallback(int /* event */, int x, int y, int /* flags */, void* /* userdata */) {
     mouseX = x;
     mouseY = y;
 }
@@ -52,7 +52,7 @@ void ThermalTemp::subCB(const sensor_msgs::ImageConstPtr& img) {
     bool putTextLeft = mouseX > colormapped.cols / 2;
     cv::putText(colormapped, text, cv::Point(putTextLeft ? mouseX - 100 : mouseX + 10, mouseY - 10), cv::FONT_HERSHEY_SIMPLEX, 0.5, textColor, 1);
     cv::imshow(tempWindow, colormapped);
-    int key = cv::waitKey(1);
+    cv::waitKey(1);
     sensor_msgs::Image outMsg;
     cv_bridge::CvImage(img->header, sensor_msgs::image_encodings::BGR8, colormapped).toImageMsg(outMsg);
 
