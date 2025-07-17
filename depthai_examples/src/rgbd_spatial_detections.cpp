a607e53395d1acfa9ed478b4128785f26825b08f
Serafadam <adam.serafin@luxonis.com>
2025-07-17 16:33:55 +0000

    stereo_inertial_node -> rgbd spatial detections

--- depthai_examples/src/rgbd_spatial_detections.cpp
+++ depthai_examples/src/rgbd_spatial_detections.cpp
@@ -1,32 +1,28 @@
 #include <cstdio>
-#include "depthai/pipeline/InputQueue.hpp"
-#include "depthai/pipeline/node/host/RGBD.hpp"
 #include <functional>
-#include <iostream>
+#include <rclcpp/logging.hpp>
 #include <tuple>
 
 #include "depthai/common/CameraBoardSocket.hpp"
-#include "depthai_bridge/PointCloudConverter.hpp"
-#include "depthai_ros_msgs/msg/spatial_detection_array.hpp"
-#include "rclcpp/node.hpp"
-#include "sensor_msgs/msg/image.hpp"
-#include "sensor_msgs/msg/imu.hpp"
-
-// Inludes common necessary includes for development using depthai library
 #include "depthai/device/Device.hpp"
+#include "depthai/pipeline/InputQueue.hpp"
 #include "depthai/pipeline/MessageQueue.hpp"
 #include "depthai/pipeline/Pipeline.hpp"
 #include "depthai/pipeline/node/IMU.hpp"
 #include "depthai/pipeline/node/SpatialDetectionNetwork.hpp"
 #include "depthai/pipeline/node/StereoDepth.hpp"
+#include "depthai/pipeline/node/host/RGBD.hpp"
 #include "depthai_bridge/BridgePublisher.hpp"
 #include "depthai_bridge/ImageConverter.hpp"
 #include "depthai_bridge/ImuConverter.hpp"
+#include "depthai_bridge/PointCloudConverter.hpp"
 #include "depthai_bridge/SpatialDetectionConverter.hpp"
 #include "depthai_bridge/TFPublisher.hpp"
+#include "depthai_ros_msgs/msg/spatial_detection_array.hpp"
+#include "rclcpp/node.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
-
-std::vector<std::string> usbStrings = {"UNKNOWN", "LOW", "FULL", "HIGH", "SUPER", "SUPER_PLUS"};
 
 struct OutputQueues {
     std::shared_ptr<dai::MessageQueue> stereoOut;
@@ -51,20 +47,15 @@ struct PipelineOpts {
     int rgbHeight;
     int monoWidth;
     int monoHeight;
-    int confidence;
-    int lrCheckThresh;
 };
 OutputQueues createPipeline(dai::Pipeline& pipeline, PipelineOpts opts) {
+    auto camRgb = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_A);
     auto monoLeft = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_B, {}, opts.stereoFPS);
     auto monoRight = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_C, {}, opts.stereoFPS);
     auto stereo = pipeline.create<dai::node::StereoDepth>();
     auto rgbd = pipeline.create<dai::node::RGBD>()->build();
     auto imu = pipeline.create<dai::node::IMU>();
-    OutputQueues queues;
-    rgbd->setDepthUnit(dai::StereoDepthConfig::AlgorithmControl::DepthUnit::METER);
-
-    queues.controlLeft = monoLeft->inputControl.createInputQueue(8, false);
-    queues.controlRight = monoRight->inputControl.createInputQueue(8, false);
+    auto spatialDetectionNetwork = pipeline.create<dai::node::SpatialDetectionNetwork>();
 
     // StereoDepth
     stereo->setRectifyEdgeFillColor(0);  // black, to better see the cutout
@@ -72,42 +63,44 @@ OutputQueues createPipeline(dai::Pipeline& pipeline, PipelineOpts opts) {
     stereo->setExtendedDisparity(opts.extended);
     stereo->setSubpixel(opts.subpixel);
 
+    rgbd->setDepthUnit(dai::StereoDepthConfig::AlgorithmControl::DepthUnit::METER);
+
     // Imu
     imu->enableIMUSensor(dai::IMUSensor::ACCELEROMETER_RAW, 500);
     imu->enableIMUSensor(dai::IMUSensor::GYROSCOPE_RAW, 400);
     imu->setBatchReportThreshold(5);
     imu->setMaxBatchReports(20);  // Get one message only for now.
-    queues.imuOut = imu->out.createOutputQueue(8, false);
 
-    // RGB image
-    auto camRgb = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_A);
-    auto rgbOut = camRgb->requestOutput(std::pair<int, int>(opts.rgbWidth, opts.rgbHeight), dai::ImgFrame::Type::RGB888i, dai::ImgResizeMode::CROP, opts.stereoFPS, true);
-    queues.rgbOut = rgbOut->createOutputQueue(8, false);
-    queues.controlRgb = camRgb->inputControl.createInputQueue(8, false);
+    auto rgbOut =
+        camRgb->requestOutput(std::pair<int, int>(opts.rgbWidth, opts.rgbHeight), dai::ImgFrame::Type::RGB888i, dai::ImgResizeMode::CROP, opts.stereoFPS, true);
+
+    auto monoOutLeft = monoLeft->requestOutput(std::make_pair(opts.monoWidth, opts.monoHeight));
+    monoOutLeft->link(stereo->left);
+    auto monoOutRight = monoRight->requestOutput(std::make_pair(opts.monoWidth, opts.monoHeight));
+    monoOutRight->link(stereo->right);
+    auto stereoOut = stereo->depth.createOutputQueue(8, false);
     rgbOut->link(stereo->inputAlignTo);
 
     stereo->depth.link(rgbd->inDepth);
     rgbOut->link(rgbd->inColor);
 
-    auto spatialDetectionNetwork = pipeline.create<dai::node::SpatialDetectionNetwork>();
     spatialDetectionNetwork->setBoundingBoxScaleFactor(0.5f);
     spatialDetectionNetwork->setDepthLowerThreshold(100);
     spatialDetectionNetwork->setDepthUpperThreshold(5000);
     dai::NNModelDescription modelDesc;
     modelDesc.model = opts.nnName;
     spatialDetectionNetwork->build(camRgb, stereo, modelDesc, 30);  // 30 FPS
+
+    OutputQueues queues;
     queues.detectionOut = spatialDetectionNetwork->out.createOutputQueue(8, false);
     queues.previewOut = spatialDetectionNetwork->passthrough.createOutputQueue(8, false);
     queues.pclOut = rgbd->pcl.createOutputQueue(8, false);
-    stereo->setRectifyEdgeFillColor(0);
-    auto monoOutLeft = monoLeft->requestOutput(std::make_pair(opts.monoWidth, opts.monoHeight));
-    monoOutLeft->link(stereo->left);
-    auto monoOutRight = monoRight->requestOutput(std::make_pair(opts.monoWidth, opts.monoHeight));
-    monoOutRight->link(stereo->right);
-
-    std::shared_ptr<dai::MessageQueue> stereoOut;
-    stereoOut = stereo->depth.createOutputQueue(8, false);
     queues.stereoOut = stereoOut;
+    queues.rgbOut = rgbOut->createOutputQueue(8, false);
+    queues.imuOut = imu->out.createOutputQueue(8, false);
+    queues.controlRgb = camRgb->inputControl.createInputQueue(8, false);
+    queues.controlLeft = monoLeft->inputControl.createInputQueue(8, false);
+    queues.controlRight = monoRight->inputControl.createInputQueue(8, false);
 
     return queues;
 }
@@ -117,6 +110,7 @@ int main(int argc, char** argv) {
     auto node = rclcpp::Node::make_shared("stereo_inertial_node");
 
     std::string mxId = node->declare_parameter<std::string>("mxId", "");
+    std::string ip = node->declare_parameter<std::string>("ip", "");
     std::string tfPrefix = node->declare_parameter<std::string>("tfPrefix", "oak");
     std::string nnName = node->declare_parameter<std::string>("nnName", "yolov6-nano");
     int imuModeParam = node->declare_parameter<int>("imuMode", 1);
@@ -128,8 +122,6 @@ int main(int argc, char** argv) {
     int monoWidth = node->declare_parameter<int>("monoWidth", 640);
     int monoHeight = node->declare_parameter<int>("monoHeight", 400);
     int stereoFPS = node->declare_parameter<int>("stereoFPS", 30);
-    int confidence = node->declare_parameter<int>("confidence", 200);
-    int lrCheckThresh = node->declare_parameter<int>("lrCheckThresh", 5);
     bool manualExposure = node->declare_parameter<bool>("manualExposure", false);
     int expTime = node->declare_parameter<int>("expTime", 20000);
     int sensIso = node->declare_parameter<int>("sensIso", 800);
@@ -142,12 +134,28 @@ int main(int argc, char** argv) {
     double enableRosBaseTimeUpdate = node->declare_parameter<bool>("enableRosBaseTimeUpdate", false);
 
     depthai_bridge::ImuSyncMethod imuMode = static_cast<depthai_bridge::ImuSyncMethod>(imuModeParam);
-    std::shared_ptr<dai::Device> device;
+    dai::DeviceInfo info;
+    if(!mxId.empty()) {
+        info = dai::DeviceInfo(mxId);
+    } else if(!ip.empty()) {
+        info = dai::DeviceInfo(ip);
+    }
 
-    device = std::make_shared<dai::Device>();
+
+    auto infos = dai::Device::getAllAvailableDevices();
+    RCLCPP_INFO(node->get_logger(), "Devices found: %zu", infos.size());
+    for(auto& deviceInfo : infos) {
+        RCLCPP_INFO(node->get_logger(), "Found device: %s", deviceInfo.getDeviceId().c_str());
+    };
+    if(mxId.empty() || ip.empty()) {
+        RCLCPP_INFO(node->get_logger(), "Opening first available device");
+    } else {
+        RCLCPP_INFO(node->get_logger(), "Opening device: %s", info.getDeviceId().c_str());
+    }
+    auto device = std::make_shared<dai::Device>(info);
 
     dai::Pipeline pipeline(device);
-    PipelineOpts opts = {nnName, lrcheck, extended, subpixel, stereoFPS, rgbWidth, rgbHeight, monoWidth, monoHeight, confidence, lrCheckThresh};
+    PipelineOpts opts = {nnName, lrcheck, extended, subpixel, stereoFPS, rgbWidth, rgbHeight, monoWidth, monoHeight};
     auto queues = createPipeline(pipeline, opts);
 
     // Set manual exposure
@@ -172,14 +180,8 @@ int main(int argc, char** argv) {
         }
     }
 
-    depthai_bridge::ImageConverter rightConverter(tfPrefix + "_right_camera_optical_frame", true);
-    if(enableRosBaseTimeUpdate) {
-        rightConverter.setUpdateRosBaseTimeOnToRosMsg();
-    }
-
     depthai_bridge::ImuConverter imuConverter(tfPrefix + "_imu_frame", imuMode, linearAccelCovariance, angularVelCovariance);
     if(enableRosBaseTimeUpdate) {
-        imuConverter.setUpdateRosBaseTimeOnToRosMsg();
     }
     depthai_bridge::BridgePublisher<sensor_msgs::msg::Imu, dai::IMUData> imuPublish(
         queues.imuOut,
@@ -194,60 +196,34 @@ int main(int argc, char** argv) {
 
     depthai_bridge::ImageConverter rgbConverter(tfPrefix + "_rgb_camera_optical_frame", false);
     if(enableRosBaseTimeUpdate) {
+        imuConverter.setUpdateRosBaseTimeOnToRosMsg();
         rgbConverter.setUpdateRosBaseTimeOnToRosMsg();
     }
     auto calibrationHandler = device->readCalibration();
-    auto pub = std::make_unique<depthai_bridge::TFPublisher>(node,
-                                    calibrationHandler,
-                                    device->getConnectedCameraFeatures(),
-                                    "oak",
-                                    device->getDeviceName(),
-                                    "oak",
-                                    "parent_frame",
-                                    "0.0",
-                                    "0,0",
-                                    "0.0",
-                                    "0.0",
-                                    "0.0",
-                                    "0.0",
-                                    "false",
-                                    "",
-                                    "",
-                                    false);
-    while(pipeline.isRunning() && rclcpp::ok()) {
+    auto tfPub = std::make_unique<depthai_bridge::TFPublisher>(node, calibrationHandler, device->getConnectedCameraFeatures(), "oak", device->getDeviceName());
+    while(rclcpp::ok() && pipeline.isRunning()) {
         depthai_bridge::PointCloudConverter pclConv(tfPrefix + "_rgb_camera_optical_frame", false);
         depthai_bridge::BridgePublisher<sensor_msgs::msg::PointCloud2, dai::PointCloudData> pclPublish(
             queues.pclOut,
             node,
             "stereo/points",
-            std::bind(&depthai_bridge::PointCloudConverter::toRosMsg,
-                      pclConv,
-                      std::placeholders::_1,
-                      std::placeholders::_2),
+            std::bind(&depthai_bridge::PointCloudConverter::toRosMsg, pclConv, std::placeholders::_1, std::placeholders::_2),
             30,
             "",
-            "pcl"
-        );
+            "pcl");
         pclPublish.addPublisherCallback();
-        auto rightCameraInfo = rightConverter.calibrationToCameraInfo(calibrationHandler, dai::CameraBoardSocket::CAM_C, monoWidth, monoHeight);
-        auto depthCameraInfo = rgbConverter.calibrationToCameraInfo(calibrationHandler, dai::CameraBoardSocket::CAM_A, rgbWidth, rgbHeight);
-        auto depthconverter = rgbConverter;
 
+        auto rgbCameraInfo = rgbConverter.calibrationToCameraInfo(calibrationHandler, dai::CameraBoardSocket::CAM_A, rgbWidth, rgbHeight);
         depthai_bridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> depthPublish(
             queues.stereoOut,
             node,
             "stereo/depth",
-            std::bind(&depthai_bridge::ImageConverter::toRosMsg,
-                      &depthconverter,  // since the converter has the same frame name
-                                        // and image type is also same we can reuse it
-                      std::placeholders::_1,
-                      std::placeholders::_2),
+            std::bind(&depthai_bridge::ImageConverter::toRosMsg, &rgbConverter, std::placeholders::_1, std::placeholders::_2),
             30,
-            depthCameraInfo,
+            rgbCameraInfo,
             "stereo");
         depthPublish.addPublisherCallback();
 
-        auto rgbCameraInfo = rgbConverter.calibrationToCameraInfo(calibrationHandler, dai::CameraBoardSocket::CAM_A, rgbWidth, rgbHeight);
         auto imgQueue = queues.rgbOut;
         depthai_bridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> rgbPublish(
             imgQueue,
