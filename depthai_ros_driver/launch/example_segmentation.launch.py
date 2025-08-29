99439b89858c768935f347183cd251b62f891b1b
Serafadam <adam.serafin@luxonis.com>
2025-08-29 12:36:14 +0000

    minor fix

--- depthai_ros_driver/launch/example_segmentation.launch.py
+++ depthai_ros_driver/launch/example_segmentation.launch.py
@@ -42,10 +42,6 @@ def generate_launch_description():
             default_value=os.path.join(depthai_prefix, "config", "segmentation.yaml"),
         ),
         DeclareLaunchArgument("use_rviz", default_value="False"),
-        DeclareLaunchArgument(
-            "rviz_config",
-            default_value=os.path.join(depthai_prefix, "config", "rviz", "rgbd.rviz"),
-        ),
     ]
 
     return LaunchDescription(
