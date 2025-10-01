ec2326b7f80fc8eff03e632c7e69a25d535c6ceb
Serafadam <adam.serafin@luxonis.com>
2025-10-01 13:36:08 +0000

    reformat

--- depthai_ros_driver/src/dai_nodes/sensors/vio.cpp
+++ depthai_ros_driver/src/dai_nodes/sensors/vio.cpp
@@ -100,7 +100,6 @@ void Vio::setupQueues(std::shared_ptr<dai::Device> /* device */) {
     odomConv->setUpdateRosBaseTimeOnToRosMsg(ph->getParam<bool>(ParamNames::UPDATE_ROS_BASE_TIME_ON_ROS_MSG));
     odomConv->setCovariance(ph->getParam<std::vector<double>>("i_covariance"));
 
-
     odomPub = getROSNode()->create_publisher<nav_msgs::msg::Odometry>("~/" + getName() + "/odometry", ph->getParam<int>(ParamNames::MAX_Q_SIZE), options);
     transQ->addCallback(std::bind(&Vio::transCB, this, std::placeholders::_1, std::placeholders::_2));
 }
