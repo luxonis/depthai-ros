84afea73771c7972ce925cec3d2c30e44379bc5d
Serafadam <adam.serafin@luxonis.com>
2025-10-07 12:01:55 +0000

    reformat

--- depthai_ros_driver/src/param_handlers/slam_param_handler.cpp
+++ depthai_ros_driver/src/param_handlers/slam_param_handler.cpp
@@ -62,7 +62,6 @@ void SlamParamHandler::declareParams(std::shared_ptr<dai::node::RTABMapSLAM> sla
     std::map<std::string, std::string> params;
     std::string configPath = declareAndLogParam<std::string>("i_config_path", "depthai_ros_driver_default_rtabmap.json");
     if(configPath.find("depthai_ros_driver_default_rtabmap.json") != std::string::npos) {
-
         configPath = ament_index_cpp::get_package_share_directory("depthai_ros_driver") + "/config/custom/" + configPath;
     }
     auto finalPath = declareAndLogParam<std::string>("config_path", configPath, true);
--- depthai_ros_driver/src/param_handlers/vio_param_handler.cpp
+++ depthai_ros_driver/src/param_handlers/vio_param_handler.cpp
@@ -1,4 +1,5 @@
 #include "depthai_ros_driver/param_handlers/vio_param_handler.hpp"
+
 #include <ament_index_cpp/get_package_share_directory.hpp>
 
 #include "depthai/basalt/BasaltVIO.hpp"
