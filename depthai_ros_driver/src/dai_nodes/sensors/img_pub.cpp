96f99b5a387a3eff489c4473ee6605b107bc85aa
Jan Hernas <hernasj@wp.pl>
2025-10-29 12:35:01 +0100

    Add uncompressed image flipping fucntionality

--- depthai_ros_driver/src/dai_nodes/sensors/img_pub.cpp
+++ depthai_ros_driver/src/dai_nodes/sensors/img_pub.cpp
@@ -10,6 +10,9 @@
 #include "depthai_ros_driver/dai_nodes/sensors/sensor_helpers.hpp"
 #include "depthai_ros_driver/utils.hpp"
 #include "image_transport/image_transport.hpp"
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <cv_bridge/cv_bridge.hpp>
 
 namespace depthai_ros_driver {
 namespace dai_nodes {
@@ -193,6 +196,26 @@ std::shared_ptr<Image> ImagePublisher::convertData(const std::shared_ptr<dai::AD
     return img;
 }
 void ImagePublisher::publish(std::shared_ptr<Image> img) {
+    // Rotate only uncompressed images - compressed image rotation is too costly
+    if(pubConfig.flipImage && !pubConfig.publishCompressed) {
+        try {
+            if(img->image) {
+                auto imgPtr = std::make_shared<sensor_msgs::msg::Image>(*img->image);
+                cv_bridge::CvImagePtr cvPtr = cv_bridge::toCvCopy(imgPtr, imgPtr->encoding);
+                cv::Mat rotated;
+                cv::flip(cvPtr->image, rotated, -1);
+                cv_bridge::CvImage outCv;
+                outCv.header = imgPtr->header;
+                outCv.encoding = imgPtr->encoding;
+                outCv.image = rotated;
+                sensor_msgs::msg::Image newMsg;
+                outCv.toImageMsg(newMsg);
+                img->image = std::make_unique<sensor_msgs::msg::Image>(newMsg);
+            }
+        } catch(const std::exception& e) {
+            RCLCPP_WARN(node->get_logger(), "Failed to rotate image before publish: %s", e.what());
+        }
+    }
     if(pubConfig.publishCompressed) {
         if(encConfig.profile == dai::VideoEncoderProperties::Profile::MJPEG) {
             compressedImgPub->publish(std::move(img->compressedImg));
