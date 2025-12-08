1ce654de1617f2b7a3934c7b7f924e95be4096da
Martin Pecka <peckama2@fel.cvut.cz>
2025-12-08 22:01:02 +0100

    Allow substituting launch args in camera launch file

--- depthai_ros_driver/launch/driver.launch.py
+++ depthai_ros_driver/launch/driver.launch.py
@@ -11,7 +11,7 @@ from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes, Node
-from launch_ros.descriptions import ComposableNode
+from launch_ros.descriptions import ComposableNode, ParameterFile
 from launch.events import Shutdown
 
 
@@ -67,7 +67,7 @@ def launch_setup(context, *args, **kwargs):
         "publish_tf_from_calibration", default="true"
     )
     override_cam_model = LaunchConfiguration("override_cam_model", default="false")
-    params_file = LaunchConfiguration("params_file")
+    params_file = ParameterFile(LaunchConfiguration("params_file"), allow_substs="true")
     camera_model = LaunchConfiguration("camera_model", default="OAK-D")
     rs_compat = LaunchConfiguration("rs_compat", default="false")
     pointcloud_enable = LaunchConfiguration("pointcloud.enable", default="false")
