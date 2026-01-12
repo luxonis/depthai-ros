f61574366e245654cc4820d19085b1d78fd3fec9
Martin Pecka <peckama2@fel.cvut.cz>
2026-01-12 02:59:29 +0100

    Fixed camera info for low_bandwidth streams.

--- depthai_ros_driver/src/dai_nodes/sensors/img_pub.cpp
+++ depthai_ros_driver/src/dai_nodes/sensors/img_pub.cpp
@@ -158,8 +158,8 @@ std::shared_ptr<Image> ImagePublisher::convertData(const std::shared_ptr<dai::AD
     auto img = std::make_shared<Image>();
     if(encConfig.enabled) {
         auto daiImg = std::dynamic_pointer_cast<dai::EncodedFrame>(data);
+        info = converter->generateCameraInfo(daiImg);
         if(pubConfig.publishCompressed) {
-            info = converter->generateCameraInfo(daiImg);
             auto rawMsg = converter->toRosMsgRawPtr(daiImg, info);
             info.header = rawMsg.header;
             if(encConfig.profile == dai::VideoEncoderProperties::Profile::MJPEG) {
@@ -173,6 +173,7 @@ std::shared_ptr<Image> ImagePublisher::convertData(const std::shared_ptr<dai::AD
             }
         } else {
             auto rawMsg = converter->toRosMsgRawPtr(daiImg, info);
+            info.header = rawMsg.header;
             sensor_msgs::msg::Image::UniquePtr msg = std::make_unique<sensor_msgs::msg::Image>(rawMsg);
             img->image = std::move(msg);
         }
